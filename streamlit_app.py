2704d43878efa9ba56fb25cad89b864843e4ac1a
Pedro-estagio <pedrolimae3@gmail.com>
2024-12-16 12:37:58 -0300

    1.2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+st.header("Relatório Divpat")
+st.text("Acompanhamento do exercício 2024")
+st.text("Chefe de divisão: Mauro Sergio Barcellos")
 
 
-st.header("Dashboard Divpat 2024")
-st.text("Acompanhamento do exercício 2024 e projeção")
+
+
+
